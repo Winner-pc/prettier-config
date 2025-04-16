ce6dcfc723b981eedcdc1d3a27c91b3cf63c0e44
DevMaffi <devmaffi550@gmail.com>
2025-04-16 18:41:11 +0300

    build: fix esm config

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup"
 export default defineConfig({
     format: ["cjs", "esm"],
     entry: ["./src/index.ts"],
-    legacyOutput: true,
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
