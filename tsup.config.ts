fabf54f02ce05b9ca8c07290b30d283a12697963
DevMaffi <devmaffi550@gmail.com>
2025-04-25 16:42:25 +0300

    build: optimize package bundle

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-    format: ["cjs", "esm"],
+    format: ["esm"],
     entry: ["./src/index.ts"],
     dts: true,
     shims: true,
     skipNodeModulesBundle: true,
+    minify: true,
     clean: true,
 })
